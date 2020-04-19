2784f6b4e579748a54fc8c6d34aa5e2d143d3d0f
Alpaca53382 <notnat.tlikehis@gmail.com>
2020-04-18 22:47:26 -0700

    portraits etc in gui

--- interface/scripted/vappysettings/vappysettings.lua
+++ interface/scripted/vappysettings/vappysettings.lua
@@ -1,4 +1,6 @@
 local vappy
+local firstOccupant
+local secondOccupant
 local bellyeffects = {
 	[-1] = "", [0] = "heal", [1] = "digest", [2] = "softdigest",
 	[""] = -1, ["heal"] = 0, ["digest"] = 1, ["softdigest"] = 2 -- reverse lookup
@@ -8,30 +10,37 @@ local clickmodes = {
 	["attack"] = -1, ["build"] = 0
 
 }
-local rpcGet
-local sentInitial = false
 
 function init()
 	vappy = config.getParameter( "vappy" )
+	firstOccupant = config.getParameter( "firstOccupant" )
+	if firstOccupant ~= nil then
+		setPortrait( "firstOccupant", world.entityPortrait( firstOccupant, "bust" ) )
+		widget.setText( "firstOccupant.name", world.entityName( firstOccupant ) )
+	else
+		widget.setButtonEnabled( "firstOccupant.letOut", false )
+	end
+	secondOccupant = config.getParameter( "secondOccupant" )
+	if secondOccupant ~= nil then
+		setPortrait( "secondOccupant", world.entityPortrait( secondOccupant, "bust" ) )
+		widget.setText( "secondOccupant.name", world.entityName( secondOccupant ) )
+	else
+		widget.setButtonEnabled( "secondOccupant.letOut", false )
+	end
 	widget.setChecked( "autoDeploy", player.getProperty( "vappyAutoDeploy" ) or false )
 	widget.setChecked( "defaultSmall", player.getProperty( "vappyDefaultSmall" ) or false )
+	widget.setSelectedOption( "bellyEffect", bellyeffects[player.getProperty("vappyBellyEffect") or ""] )
+	widget.setSelectedOption( "clickMode", clickmodes[player.getProperty("vappyClickMode") or "attack"] )
 end
 
 function update( dt )
-	if not sentInitial then
-		rpcGet = world.sendEntityMessage( vappy, "settingsMenuGet" )
-		sentInitial = true
-	elseif rpcGet ~= nil and rpcGet:finished() then
-		if rpcGet:succeeded() then
-			local result = rpcGet:result()
-			widget.setSelectedOption( "bellyEffect", bellyeffects[result.bellyeffect] )
-			widget.setSelectedOption( "clickMode", clickmodes[result.clickmode] )
-		else
-			sb.logError( "Couldn't get data for Vappy settings menu." )
-			sb.logError( rpcGet:error() )
-		end
-		rpcGet = nil
-		-- rpcGet = world.sendEntityMessage( vappy, "settingsMenuGet" )
+	if firstOccupant ~= nil and world.entityExists( firstOccupant ) then
+		local firstOccupantHealth = world.entityHealth( firstOccupant )
+		widget.setProgress( "firstOccupant.healthbar", firstOccupantHealth[1] / firstOccupantHealth[2] )
+	end
+	if secondOccupant ~= nil and world.entityExists( secondOccupant ) then
+		local secondOccupantHealth = world.entityHealth( secondOccupant )
+		widget.setProgress( "secondOccupant.healthbar", secondOccupantHealth[1] / secondOccupantHealth[2] )
 	end
 end
 
@@ -58,4 +67,15 @@ end
 
 function despawn()
 	world.sendEntityMessage( vappy, "despawn" )
+end
+
+function setPortrait( canvasName, data )
+	local canvas = widget.bindCanvas( canvasName..".portrait" )
+	canvas:clear()
+	for k,v in ipairs(data) do
+		canvas:drawImage(v.image, { -7, -19 } )
+	end
+end
+function letOut(_, which )
+	world.sendEntityMessage( vappy, "settingsMenuSet", "letout", which )
 end
