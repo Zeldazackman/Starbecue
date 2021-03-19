08ccc3e969564c3a12abcd6020da7def993a90fd
WasabiRaptor <ketchupraptor@gmail.com>
2021-03-19 19:53:10 -0400

    fix succ persisting and not allowing a refresh of position

--- stats/playablevsoeffects/succ/succ.lua
+++ stats/playablevsoeffects/succ/succ.lua
@@ -10,12 +10,13 @@ function init()
 end
 
 function update(dt)
-	if not distance then return end
+	if not distance then effect.expire() return end
 	if distance > 1 then
 		mcontroller.controlApproachVelocity({50 * x/distance, 50 * y/distance}, 650/(distance^0.5))
 	else
 		mcontroller.setVelocity({10 * x, 10 * y})
 	end
+	effect.expire()
 end
 
 function uninit()
