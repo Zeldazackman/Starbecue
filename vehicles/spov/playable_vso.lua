c934683722d50bce5f06a0e20ed492b635aff237
WasabiRaptor <ketchupraptor@gmail.com>
2021-08-20 07:41:30 -0400

    yep, that whole block of code is obsolete now

--- vehicles/spov/playable_vso.lua
+++ vehicles/spov/playable_vso.lua
@@ -992,86 +992,6 @@ function p.control.pressE(_,_, seat_index )
 	end
 end
 
-function p.control.interact()
-	if p.movement.E then -- intercepting vsoForcePlayerSit to get this
-		if not p.movement.wasE then
-			local aim = vehicle.aimPosition( p.control.driver )
-			local mpos = mcontroller.position()
-			local dpos = world.distance( mpos, aim )
-			local interactables
-			local queryParameters = {
-				withoutEntityId = entity.id(), -- don't interact with self
-				boundMode = "collisionarea", -- hopefully this makes you not have to point at the exact center of the object... doubtful though
-				order = "nearest"
-			}
-			if world.magnitude( dpos ) < 9 then -- interact range -- and not world.lineTileCollision( mpos, aim )
-				interactables = world.entityQuery( aim, 0.5, queryParameters )
-			else
-				interactables = world.entityQuery( mcontroller.position(), 3, queryParameters )
-			end
-			local obj = interactables[1]
-			local driver = vehicle.entityLoungingIn( p.control.driver )
-			if obj == driver then
-				obj = interactables[2]
-			end
-			if obj ~= nil and driver ~= nil then
-				local objpos = world.entityPosition( obj )
-				vsoDebugRect( objpos[1]-0.5, objpos[2]-0.5, objpos[1]+0.5, objpos[2]+0.5, "red" )
-
-				local name = world.getObjectParameter( obj, "objectName" )
-				-- if name ~= nil then -- object
-					local interactaction = world.getObjectParameter( obj, "interactAction" )
-					local interactdata = world.getObjectParameter( obj, "interactData" )
-					local localinteracted = false
-					if interactaction == nil then -- some things return that from script? let's try to get that
-						local s, e = pcall(function() -- this only works on local entities, pcall should stop it from crashing the game
-							local action = world.callScriptedEntity( obj, "onInteraction", {
-								source = world.distance( mpos, objpos ),
-								sourceId = driver
-							} )
-							if action ~= nil then
-								interactaction = action[1]
-								interactdata = action[2]
-							end
-							localinteracted = true
-						end)
-						if not s then
-							sb.logError(e)
-						end
-					end
-					if interactaction ~= nil then
-						if type( interactdata ) == "string" then
-							interactdata = root.assetJson( interactdata )
-						end
-						world.sendEntityMessage( driver, "vsoForceInteract", interactaction, interactdata, obj )
-					elseif world.getObjectParameter( obj, "uiConfig" ) ~= nil then
-						uiconfig = world.getObjectParameter( obj, "uiConfig" )
-						if world.getObjectParameter( obj, "slotCount" ) ~= nil then
-							uiconfig = sb.replaceTags( uiconfig, { ["slots"] = world.getObjectParameter( obj, "slotCount" ) } )
-						end
-						local configdata = root.assetJson( uiconfig )
-						world.sendEntityMessage( driver, "vsoForceInteract", "OpenContainer", configdata, obj )
-					elseif world.getObjectParameter( obj, "upgradeStates" ) then -- upgradeablecraftingobjects
-					elseif not localinteracted then -- call onInteraction for non-local entities, sadly we can't get the return value or this would be earlier
-						world.objectQuery( objpos, 1, {
-							name = name,
-							callScript = "onInteraction",
-							callScriptArgs = { {
-								source = world.distance( mpos, objpos ),
-								sourceId = driver
-							} },
-						} )
-					end
-				-- end
-			end
-		end
-		p.movement.wasE = true
-	else
-		p.movement.wasE = false
-	end
-	p.movement.E = false
-end
-
 function p.control.drive()
 	if not p.control.driving then return end
 	local control = p.stateconfig[p.state].control
@@ -1095,7 +1015,6 @@ function p.control.drive()
 
 	p.control.primaryAction()
 	p.control.altAction()
-	p.control.interact()
 end
 
 function p.control.primaryAction()
