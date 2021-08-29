3c3834e3fe7b115fbe68a61cd2c756394d0a20a8
WasabiRaptor <ketchupraptor@gmail.com>
2021-08-29 18:28:31 -0400

    rename to make things clearer which is a companion and which is a primary script

--- scripts/ZSSVMA_player.lua
+++ scripts/ZSSVMA_player.lua
@@ -1,44 +1,6 @@
 local oldinit = init
 function init()
 	oldinit()
-	message.setHandler( "loadVSOsettings", function(_,_, vsoMenuName )
-		local settings = player.getProperty( "vsoSettings" ) or {}
-		if vsoMenuName then return settings[vsoMenuName] or {} end
-		return settings
-	end)
-	message.setHandler( "saveVSOsettings", function(_,_, settings )
-		player.setProperty( "vsoSettings", settings )
-	end)
-	message.setHandler( "openPVSOInterface", function(_,_, name, args, appendSettings, sourceEntity)
-		local pane = root.assetJson("/interface/scripted/pvso/"..name.."/"..name..".config")
-		if args then
-			pane = sb.jsonMerge(pane, args)
-		end
-		if appendSettings and appendSettings ~= "" then
-			pane.settings = player.getProperty( "vsoSettings", {} )
-			if type(appendSettings) == "string" then
-				pane.settings = pane.settings[appendSettings] or {}
-			end
-		end
-		player.interact("ScriptPane", pane, sourceEntity or entity.id())
-	end)
-	message.setHandler( "isLounging", function()
-		return player.isLounging(), player.loungingIn()
-	end)
-
-	message.setHandler("spawnSmolPrey", function(_,_, species )
-		local position = world.entityPosition( entity.id() )
-		local settings = player.getProperty( "vsoSettings", {} )[species] or {}
-		world.spawnVehicle( "spov"..species, { position[1], position[2] + 1.5 }, { driver = entity.id(), settings = settings, uneaten = true, data = species } )
-	end )
-
-	message.setHandler("useEnergy", function( _, _, energyUsed)
-		return status.overConsumeResource("energy", energyUsed)
-	end )
-
-	message.setHandler("getDriverStat", function( _, _, stat)
-		return status.stat(stat)
-	end )
 
 	message.setHandler("getVSOseatInformation", function()
 		local seatdata = {
@@ -58,45 +20,5 @@ function init()
 		}
 		return seatdata
 	end)
-
-	message.setHandler("addHungerHealth", function( _, _, amount)
-		if status.resourcePercentage("food") < 1 then
-			status.modifyResourcePercentage( "food", amount)
-			return 1
-		elseif status.resourcePercentage("health") < 1 then
-			status.modifyResourcePercentage( "health", amount)
-			return 2
-		else
-			return 3
-		end
-	end )
-
-	message.setHandler("unlockVSO", function(_,_, name )
-		local settings = player.getProperty( "vsoSettings" ) or {}
-		if settings.vsos == nil then settings.vsos = {} end
-		if not settings.vsos[name] then
-			settings.vsos[name] = {}
-		end
-		player.setProperty( "vsoSettings", settings )
-	end)
-
-	message.setHandler("getRadialSelection", function(_,_, stat)
-		return player.getProperty("radialSelection")
-	end)
-
-	message.setHandler("pvsoApplyStatusEffects", function(_,_, effects, source)
-		status.addEphemeralEffects(effects, source)
-	end)
-
-	message.setHandler("pvsoRemoveStatusEffect", function(_,_, effect)
-		status.removeEphemeralEffect(effect)
-	end)
-
-	message.setHandler("pvsoRemoveStatusEffects", function(_,_, effects)
-		for i = 1, #effects do
-			status.removeEphemeralEffect(effect[i])
-		end
-	end)
-
 
 end
