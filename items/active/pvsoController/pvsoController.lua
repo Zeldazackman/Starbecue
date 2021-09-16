36436546204f1ceacb54b5de15dc3818fe82f5ba
WasabiRaptor <ketchupraptor@gmail.com>
2021-09-16 14:05:19 -0400

    make grab and tailvore use the generic colors, add icons for when unassigned and for when its any vore action

--- items/active/pvsoController/pvsoController.lua
+++ items/active/pvsoController/pvsoController.lua
@@ -37,9 +37,11 @@ function update(dt, fireMode, shiftHeld, controls)
 			storage.clickAction = "vore"
 		else
 			getNextAction()
-			if storage.clickAction ~= nil then
-				activeItem.setInventoryIcon("/items/active/pvsoController/"..storage.clickAction..".png")
-			end
+		end
+		if storage.clickAction ~= nil then
+			activeItem.setInventoryIcon("/items/active/pvsoController/"..storage.clickAction..".png")
+		else
+			activeItem.setInventoryIcon("/items/active/pvsoController/unassigned.png")
 		end
 	elseif fireMode ~= "primary" then
 		clicked = false
