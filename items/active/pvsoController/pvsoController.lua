5d54b5752043e2a05756f52c1e3d5b85a905305f
WasabiRaptor <ketchupraptor@gmail.com>
2021-09-16 01:34:42 -0400

    very rudimentary placeholder system for cycling the vore controller type before I put effort into the UI

--- items/active/pvsoController/pvsoController.lua
+++ items/active/pvsoController/pvsoController.lua
@@ -3,6 +3,10 @@ function init()
 	local hand = activeItem.hand()
 
 	message.setHandler( hand.."ItemData", function(_,_, data)
+		if data.assignClickAction ~= nil then
+			storage.clickAction = data.assignClickAction
+			activeItem.setInventoryIcon("/items/active/pvsoController/"..data.assignClickAction..".png")
+		end
 		if not storage.clickAction and data.defaultClickAction ~= nil then
 			activeItem.setInventoryIcon("/items/active/pvsoController/"..data.defaultClickAction..".png")
 		end
@@ -12,6 +16,41 @@ function init()
 	end
 end
 
-function update()
+local voreActionList = {
+	"vore",
+	"oralVore",
+	"analVore",
+	"tailVore",
+	"cockVore",
+	"breastVore",
+	"physicalAttack",
+	"specialAttack",
+	"grab",
+	"succ"
+}
 
+function update(dt, fireMode, shiftHeld, controls)
+	if not player.isLounging() and fireMode == "primary" and not clicked then
+		sb.logInfo("clicky")
+		clicked = true
+		if storage.clickAction == nil then
+			storage.clickAction = "vore"
+		else
+			getNextAction()
+			if storage.clickAction ~= nil then
+				activeItem.setInventoryIcon("/items/active/pvsoController/"..storage.clickAction..".png")
+			end
+		end
+	elseif fireMode ~= "primary" then
+		clicked = false
+	end
+end
+
+function getNextAction()
+	for i, action in ipairs(voreActionList) do
+		if storage.clickAction == action then
+			storage.clickAction = voreActionList[i+1]
+			return
+		end
+	end
 end
