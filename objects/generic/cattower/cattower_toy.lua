08ef8ceb5cc727a45b70140209b0a3f9633521b8
Alpaca53382 <notnat.tlikehis@gmail.com>
2021-10-03 21:24:19 -0400

    interactive dangly toys

---
+++ objects/generic/cattower/cattower_toy.lua
@@ -0,0 +1,14 @@
+function init()
+    object.setInteractive(true)
+    if object.direction() < 1 then
+        animator.setGlobalTag("flip", "?flipx") -- unflip the flip even though I told it not to flip in the first place
+    end
+end
+
+function onInteraction(args)
+    if args.source[1] < 0 then
+        animator.setAnimationState("toy", "right", true)
+    else
+        animator.setAnimationState("toy", "left", true)
+    end
+end
