bb222b403324e1f85652bc6b7ee467c600f31823
WasabiRaptor <ketchupraptor@gmail.com>
2021-10-07 18:31:02 -0400

    Auri eye tracking

--- objects/vore/voreShop/voreShopAuri/voreShopAuri.lua
+++ objects/vore/voreShop/voreShopAuri/voreShopAuri.lua
@@ -13,22 +13,59 @@ end
 --end
 
 function eyeTracking()
+	local X = 0
+	local Y = 0
+
 	local headPos = {-0.375, 6.75}
 	local worldHeadPos = object.toAbsolutePosition(headPos)
-	local target = getVisibleEntity(world.playerQuery(worldHeadPos, 20 ))
-	if not target then target = getVisibleEntity(world.npcQuery(worldHeadPos, 20 )) end
+	local target = getVisibleEntity(world.playerQuery(worldHeadPos, 50 ))
+	if not target then target = getVisibleEntity(world.npcQuery(worldHeadPos, 50 )) end
 
 	if target ~= nil then
-		local targetDist = entity.distanceToEntity(target)
-		world.debugLine(worldHeadPos, world.entityPosition(target), {72, 207, 180})
+		local targetPos = world.entityPosition(target)
+		local targetDist = world.distance(targetPos, worldHeadPos)
+		world.debugLine(worldHeadPos, targetPos, {72, 207, 180})
 
-		local angle = math.atan(targetDist[2] - headPos[2], targetDist[1] - headPos[1])
+		local angle = math.atan(targetDist[2], targetDist[1]) * 180/math.pi
+		local distance = world.magnitude(worldHeadPos, targetPos)
+		if distance > 1 then
+			if angle <= 15 and angle >= -15 then
+				X = 1 * object.direction()
+				Y = 0
+			elseif angle <= 75 and angle > 15 then
+				X = 1 * object.direction()
+				Y = 1
+			elseif angle <= 105 and angle > 75 then
+				X = 0
+				Y = 1
+			elseif angle <= 165 and angle > 105 then
+				X = -1 * object.direction()
+				Y = 1
+			elseif angle > 165 then
+				X = -1 * object.direction()
+				Y = 0
 
+			elseif angle >= -75 and angle < -15 then
+				X = 1 * object.direction()
+				Y = -1
+			elseif angle >= -105 and angle < -75 then
+				X = 0
+				Y = -1
+			elseif angle >= -165 and angle < -105 then
+				X = -1 * object.direction()
+				Y = -1
+			elseif angle < -165 then
+				X = -1 * object.direction()
+				Y = 0
+			end
 
-	else
-		animator.setGlobalTag("eyesX", "0")
-		animator.setGlobalTag("eyesY", "0")
+			if distance > 5 then
+				X = X * 2
+			end
+		end
 	end
+	animator.setGlobalTag("eyesX", X)
+	animator.setGlobalTag("eyesY", Y)
 end
 
 function getVisibleEntity(entities)
