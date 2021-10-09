6eb2d47fb42c2c4451a487b1e295c928381f0cf1
WasabiRaptor <ketchupraptor@gmail.com>
2021-10-08 22:29:52 -0400

    placer object placing it level with the ground

--- objects/vore/voreShop/voreShopDungeonPlacer/voreShopDungeonPlacer.lua
+++ objects/vore/voreShop/voreShopDungeonPlacer/voreShopDungeonPlacer.lua
@@ -1,5 +1,5 @@
 function init()
-	world.placeDungeon( config.getParameter("dungeon"), object.position(), config.getParameter("dungeonId") )
+	world.placeDungeon( config.getParameter("dungeon"), object.toAbsolutePosition(config.getParameter("placeOffset")) or object.position(), config.getParameter("dungeonId") )
 	world.setTileProtection( config.getParameter("dungeonId"), config.getParameter("protect") )
 	object.smash(true)
 end
