0fbc2f48c06f260d1a03a7b6a09790b59909d3c4
WasabiRaptor <ketchupraptor@gmail.com>
2021-10-10 23:10:55 -0400

    stuff to make the shop world not so bland even if its still mostly empty

--- objects/vore/voreShop/voreShopDungeonPlacer/voreShopDungeonPlacer.lua
+++ objects/vore/voreShop/voreShopDungeonPlacer/voreShopDungeonPlacer.lua
@@ -1,9 +1,9 @@
 function init()
-	world.placeDungeon( config.getParameter("dungeon"), object.toAbsolutePosition(config.getParameter("placeOffset")) or object.position(), config.getParameter("dungeonId") )
-	world.setTileProtection( config.getParameter("dungeonId"), config.getParameter("protect") )
+	world.placeDungeon( config.getParameter("dungeon"), object.toAbsolutePosition(config.getParameter("placeOffset")) or object.position(), config.getParameter("dungeonId") or 0 )
+	world.setTileProtection( config.getParameter("dungeonId") or 0, config.getParameter("protect") or false )
 	object.smash(true)
 end
 
 function die()
-	world.placeDungeon( config.getParameter("dungeon"), object.toAbsolutePosition(config.getParameter("placeOffset")) or object.position(), config.getParameter("dungeonId") )
+	world.placeDungeon( config.getParameter("dungeon"), object.toAbsolutePosition(config.getParameter("placeOffset")) or object.position(), config.getParameter("dungeonId") or 0 )
 end
