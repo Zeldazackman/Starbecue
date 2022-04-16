491b4e8b9f8e0acc2ae0c7408b4e0422d774dc21
WasabiRaptor <ketchupraptor@gmail.com>
2022-04-16 19:39:27 -0400

    add all the parts we're going to need into the animation for the future

--- humanoid/sbq_voreAnimOverrides.lua
+++ humanoid/sbq_voreAnimOverrides.lua
@@ -3,6 +3,8 @@
 --local oldInitAfterInit = initAfterInit
 --local oldDoUpdate = doUpdate
 
+sbq = {}
+
 message.setHandler("sbqUpdateAnimTags", function (_,_, animTags)
 	for tag, value in pairs(animTags) do
 		animator.setGlobalTag(tag, value)
@@ -46,6 +48,9 @@ message.setHandler("sbqGetAnimData", function (_,_, partTags)
 	return {self.animStateData, mcontroller.facingDirection()}
 end)
 
+function sbq.getSettings(callback, failcallback)
+	addRPC(world.sendEntityMessage(entity.id(), "sbqLoadSettings", "sbqOccupantHolder"), callback, failcallback)
+end
 
 -- this function need to be replaced/modified because of stuff that would be in the chest area for say, breast vore
 local _chest_addon = setCosmetic.chest_addon
@@ -68,23 +73,60 @@ end
 local _legs_addon = setCosmetic.legs_addon
 function setCosmetic.legs_addon(cosmetic, item, directives)
 	local belly = fixFilepath(item.config[self.gender.."BellyFrames"], item)
-	local cock = fixFilepath(item.config[self.gender.."CockFrames"], item)
 
 	animator.setPartTag("belly_cosmetic", "cosmeticDirectives", directives or "" )
 	animator.setPartTag("belly_cosmetic", "partImage", belly or "" )
 
-	animator.setPartTag("cock_cosmetic", "cosmeticDirectives", directives or "" )
-	animator.setPartTag("cock_cosmetic", "partImage", cock or "" )
+	sbq.getSettings(function (settings)
+		if settings.penis then
+			local cock = fixFilepath(item.config[self.gender.."CockFrames"], item)
+			animator.setPartTag("cock_cosmetic", "cosmeticDirectives", directives or "" )
+			animator.setPartTag("cock_cosmetic", "partImage", cock or "" )
+		else
+			sbq.clearPenis()
+		end
+
+		if settings.balls then
+			local ballsFront = fixFilepath(item.config[self.gender.."ballsFrontFrames"], item)
+			local ballsBack = fixFilepath(item.config[self.gender.."ballsBackFrames"], item)
+			animator.setPartTag("ballsFront_cosmetic", "cosmeticDirectives", directives or "" )
+			animator.setPartTag("ballsFront_cosmetic", "partImage", ballsFront or "" )
+			animator.setPartTag("ballsBack_cosmetic", "cosmeticDirectives", directives or "" )
+			animator.setPartTag("ballsBack_cosmetic", "partImage", ballsBack or "" )
+		else
+			sbq.clearBalls()
+		end
+
+		if settings.pussy then
+			local pussy = fixFilepath(item.config[self.gender.."pussyFrames"], item)
+			animator.setPartTag("pussy_cosmetic", "cosmeticDirectives", directives or "" )
+			animator.setPartTag("pussy_cosmetic", "partImage", pussy or "" )
+		else
+			sbq.clearPussy()
+		end
+	end)
 
 	_legs_addon(cosmetic, item, directives)
 end
 
+function sbq.clearPenis()
+	animator.setPartTag("cock_cosmetic", "partImage", "" )
+end
+function sbq.clearBalls()
+	animator.setPartTag("ballsFront_cosmetic", "partImage", "" )
+	animator.setPartTag("ballsBack_cosmetic", "partImage", "" )
+end
+function sbq.clearPussy()
+	animator.setPartTag("pussy_cosmetic", "partImage", "" )
+end
+
 local _legs_clear = setCosmetic.legs_clear
 function setCosmetic.legs_clear(cosmetic)
-	currentCosmeticName.legs = nil
 
 	animator.setPartTag("belly_cosmetic", "partImage", "" )
-	animator.setPartTag("cock_cosmetic", "partImage", "" )
+	sbq.clearPenis()
+	sbq.clearBalls()
+	sbq.clearPussy()
 
 	_legs_clear(cosmetic)
 end
