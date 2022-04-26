90a2e1b1928393b63685e89a75bcfc30c463a3dd
WasabiRaptor <ketchupraptor@gmail.com>
2022-04-25 20:35:12 -0400

    fix shop not working

--- objects/sbq/sbqShop/sbqShopNpcSpawner/sbqShopNpcSpawner.lua
+++ objects/sbq/sbqShop/sbqShopNpcSpawner/sbqShopNpcSpawner.lua
@@ -4,7 +4,6 @@ end
 
 function die()
 	local npc = config.getParameter("npc")
-	sb.logInfo(npc)
 	local success, data = pcall(root.assetJson, ("/species/"..npc..".species"))
 	if success then
 		world.spawnNpc(object.position(), npc, config.getParameter("typeName"), config.getParameter("level"), config.getParameter("seed"), config.getParameter("npcParameters") )
