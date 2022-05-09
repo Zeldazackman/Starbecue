30fe1186a08449e95a3bee125d2a4d24c6ef1385
WasabiRaptor <ketchupraptor@gmail.com>
2022-05-09 00:09:40 -0400

    fix errors on dungeon generation

--- objects/sbq/sbqShop/sbqShopDungeonPlacer/sbqShopDungeonPlacer.lua
+++ objects/sbq/sbqShop/sbqShopDungeonPlacer/sbqShopDungeonPlacer.lua
@@ -1,8 +1,15 @@
+local inited
+local updated
 function init()
-	object.smash(true)
+	inited = true
 end
-
-function die()
-	world.setTileProtection( config.getParameter("dungeonId") or 0, config.getParameter("protect") or false )
-	world.placeDungeon( config.getParameter("dungeon"), object.toAbsolutePosition(config.getParameter("placeOffset") or {0,0}), config.getParameter("dungeonId") or 0 )
+function update()
+	if inited and updated and (world.regionActive(object.boundBox())) then
+		world.setTileProtection( config.getParameter("dungeonId") or 0, config.getParameter("protect") or false )
+		world.placeDungeon( config.getParameter("dungeon"), object.toAbsolutePosition(config.getParameter("placeOffset") or {0,0}), config.getParameter("dungeonId") or 0 )
+		object.smash(true)
+	end
+	if inited then
+		updated = true
+	end
 end
