324d12bbdd3f530e0a4558e11cec9b84feb8cf65
WasabiRaptor <ketchupraptor@gmail.com>
2022-05-17 22:32:52 -0400

    args for the dungeon placer to check requirements and choose from a random selection

--- stagehands/sbq/sbqDungeonPlacer.lua
+++ stagehands/sbq/sbqDungeonPlacer.lua
@@ -2,9 +2,43 @@
 function update()
 	local position = stagehand.position()
 	if world.regionActive({position[1]-1,position[2]-1,position[1]+1,position[2]+1}) then
-		local offset = config.getParameter("placeOffset") or {0,0}
-		world.setTileProtection( config.getParameter("dungeonId") or 0, config.getParameter("protect") or false )
-		world.placeDungeon( config.getParameter("dungeon"), {position[1]+offset[1],position[2]+offset[2]}, config.getParameter("dungeonId") or 0 )
+
+		local data = config.getParameter("randomSelection") or {}
+		if type(data) == "string" then
+			data = root.assetJson(data)
+		end
+		local gotData = false
+		if data[1] == nil then
+			gotData = true
+		end
+		while not gotData do
+			local i = math.random(#data)
+			if not checkRequirements(data[i].checkRequirements or {}) then table.remove(data,i) break end
+			data = data[i]
+			gotData = true
+		end
+
+		if not checkRequirements(config.getParameter("checkRequirements") or {}) then return stagehand.die() end
+
+		local offset = data.placeOffset or config.getParameter("placeOffset") or {0,0}
+		world.setTileProtection( data.dungeonId or config.getParameter("dungeonId") or 0, data.protect or config.getParameter("protect") or false )
+		world.placeDungeon( data.dungeon or config.getParameter("dungeon"), {position[1]+offset[1],position[2]+offset[2]}, data.dungeonId or config.getParameter("dungeonId") or 0 )
 		stagehand.die()
 	end
+end
+
+function checkRequirements(data)
+	if data.checkItems then
+		for i, item in ipairs(data.checkItems) do
+			if not root.itemConfig(item) then return end
+		end
+	end
+	if data.checkJson then
+		if not pcall(root.assetJson, data.checkJson) then return end
+	end
+	if data.checkImage then
+		success, notEmpty = pcall(root.nonEmptyRegion, data.checkImage)
+		if not (success and notEmpty ~= nil) then return end
+	end
+	return true
 end
--- stagehands/sbq/sbqNPCSpawner.lua
+++ stagehands/sbq/sbqNPCSpawner.lua
@@ -2,13 +2,47 @@
 function update()
 	local position = stagehand.position()
 	if world.regionActive({position[1]-1,position[2]-1,position[1]+1,position[2]+1}) then
-		local npc = config.getParameter("npc")
+
+		local data = config.getParameter("randomSelection") or {}
+		if type(data) == "string" then
+			data = root.assetJson(data)
+		end
+		local gotData = false
+		if data[1] == nil then
+			gotData = true
+		end
+		while not gotData do
+			local i = math.random(#data)
+			if not checkRequirements(data[i].checkRequirements or {}) then table.remove(data,i) break end
+			data = data[i]
+			gotData = true
+		end
+
+		if not checkRequirements(config.getParameter("checkRequirements") or {}) then return stagehand.die() end
+
+		local npc = data.npc or config.getParameter("npc")
 		if type(npc) == "string" then
-			local success, data = pcall(root.assetJson, ("/species/" .. npc .. ".species"))
+			local success = pcall(root.assetJson, ("/species/" .. npc .. ".species"))
 			if success then
-				world.spawnNpc(position, npc, config.getParameter("npcTypeName"), config.getParameter("npcLevel") or world.threatLevel(), config.getParameter("npcSeed"), config.getParameter("npcParameters"))
+				world.spawnNpc(position, npc, data.npcTypeName or config.getParameter("npcTypeName"), data.npcLevel or config.getParameter("npcLevel") or world.threatLevel(), data.npcSeed or config.getParameter("npcSeed"), data.npcParameters or config.getParameter("npcParameters"))
 			end
 		end
 		stagehand.die()
 	end
+end
+
+function checkRequirements(data)
+	if data.checkItems then
+		for i, item in ipairs(data.checkItems) do
+			if not root.itemConfig(item) then return end
+		end
+	end
+	if data.checkJson then
+		if not pcall(root.assetJson, data.checkJson) then return end
+	end
+	if data.checkImage then
+		success, notEmpty = pcall(root.nonEmptyRegion, data.checkImage)
+		if not (success and notEmpty ~= nil) then return end
+	end
+	return true
 end
