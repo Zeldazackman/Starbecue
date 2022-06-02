b21a6c35cc957c62fd0b415b3a3f106f8dda9ae8
WasabiRaptor <ketchupraptor@gmail.com>
2022-06-02 15:52:23 -0400

    fix the detection for colony deeds, and make the vore colony deed actually detect other deeds now

--- objects/spawner/colonydeed/sbqColonyDeedDetection.lua
+++ objects/spawner/colonydeed/sbqColonyDeedDetection.lua
@@ -3,7 +3,7 @@ local _scanHouseContents = scanHouseContents
 function scanHouseContents(boundary)
 	local returnValues = _scanHouseContents(boundary)
 
-	for i, object in ipairs(returnValues.objects) do
+	for object, _ in pairs(returnValues.objects) do
 		local name = world.entityName(object)
 		if name == "sbqVoreColonyDeed" or name == "compactdeed" or name == "sbqVoreCompactDeed"then
 			returnValues.otherDeed = true
