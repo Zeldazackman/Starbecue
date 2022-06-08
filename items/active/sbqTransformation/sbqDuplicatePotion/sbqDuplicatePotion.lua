ff6a423a01dc72fb01b08432432377b7792f1132
WasabiRaptor <ketchupraptor@gmail.com>
2022-06-08 18:35:50 -0400

    fix species and duplicate potions to work if you don't already have have override data

--- items/active/sbqTransformation/sbqDuplicatePotion/sbqDuplicatePotion.lua
+++ items/active/sbqTransformation/sbqDuplicatePotion/sbqDuplicatePotion.lua
@@ -4,32 +4,32 @@ function init()
 end
 
 function getIdentity()
-	self = status.statusProperty("speciesAnimOverrideData")
-	self.gender = self.gender or world.entityGender(entity.id())
-	self.species = self.species or world.entitySpecies(entity.id())
-	self.identity = self.identity or {}
-	self.name = world.entityName(player.id())
+	local overrideData = status.statusProperty("speciesAnimOverrideData") or {}
+	overrideData.gender = overrideData.gender or world.entityGender(entity.id())
+	overrideData.species = overrideData.species or world.entitySpecies(entity.id())
+	overrideData.identity = overrideData.identity or {}
+	overrideData.name = world.entityName(player.id())
 
-	local success, speciesFile = pcall(root.assetJson, ("/species/"..self.species..".species"))
+	local success, speciesFile = pcall(root.assetJson, ("/species/"..overrideData.species..".species"))
 	if success then
-		if not self.identity.hairGroup and type(speciesFile) == "table" then
+		if not overrideData.identity.hairGroup and type(speciesFile) == "table" then
 			for i, data in ipairs(speciesFile.genders or {}) do
-				if data.name == self.gender then
-					self.identity.hairGroup = data.hairGroup or "hair"
+				if data.name == overrideData.gender then
+					overrideData.identity.hairGroup = data.hairGroup or "hair"
 				end
 			end
 		end
-		if not self.identity.facialHairGroup and type(speciesFile) == "table" then
+		if not overrideData.identity.facialHairGroup and type(speciesFile) == "table" then
 			for i, data in ipairs(speciesFile.genders or {}) do
-				if data.name == self.gender then
-					self.identity.facialHairGroup = data.facialHairGroup or "facialHair"
+				if data.name == overrideData.gender then
+					overrideData.identity.facialHairGroup = data.facialHairGroup or "facialHair"
 				end
 			end
 		end
-		if not self.identity.facialMaskGroup and type(speciesFile) == "table" then
+		if not overrideData.identity.facialMaskGroup and type(speciesFile) == "table" then
 			for i, data in ipairs(speciesFile.genders or {}) do
-				if data.name == self.gender then
-					self.identity.facialMaskGroup = data.facialMaskGroup or "facialMask"
+				if data.name == overrideData.gender then
+					overrideData.identity.facialMaskGroup = data.facialMaskGroup or "facialMask"
 				end
 			end
 		end
@@ -38,53 +38,53 @@ function getIdentity()
 		for _, part in ipairs(portrait) do
 			local imageString = part.image
 			--get personality values
-			if not self.identity.body then
+			if not overrideData.identity.body then
 				local found1, found2 = imageString:find("body.png:idle.")
 				if found1 ~= nil then
-					self.identity.body = imageString:sub(found2+1, found2+1)
+					overrideData.identity.body = imageString:sub(found2+1, found2+1)
 
 					local directives = imageString:sub(found2+2)
-					self.directives = self.directives or directives
+					overrideData.directives = overrideData.directives or directives
 				end
 			end
-			if not self.identity.arm then
+			if not overrideData.identity.arm then
 				local found1, found2 = imageString:find("backarm.png:idle.")
 				if found1 ~= nil then
-					self.identity.arm = imageString:sub(found2+1, found2+1)
+					overrideData.identity.arm = imageString:sub(found2+1, found2+1)
 				end
 			end
 
-			if not self.identity.hairType then
-				local found1, found2 = imageString:find("/"..(self.identity.hairGroup or "hair").."/")
+			if not overrideData.identity.hairType then
+				local found1, found2 = imageString:find("/"..(overrideData.identity.hairGroup or "hair").."/")
 				if found1 ~= nil then
 					local found3, found4 = imageString:find(".png:normal")
-					self.identity.hairType = imageString:sub(found2+1, found3-1)
+					overrideData.identity.hairType = imageString:sub(found2+1, found3-1)
 
 					local found5, found6 = imageString:find("?addmask=")
 					local hairDirectives = imageString:sub(found4+1, (found5 or 0)-1) -- this is really elegant haha
 
-					self.hairDirectives = self.hairDirectives or hairDirectives
+					overrideData.hairDirectives = overrideData.hairDirectives or hairDirectives
 				end
 			end
 
-			if not self.identity.facialHairType then
-				local found1, found2 = imageString:find("/"..(self.identity.facialHairGroup or "facialHair").."/")
+			if not overrideData.identity.facialHairType then
+				local found1, found2 = imageString:find("/"..(overrideData.identity.facialHairGroup or "facialHair").."/")
 				if found1 ~= nil then
 					found3, found4 = imageString:find(".png")
-					self.identity.facialHairType = imageString:sub(found2+1, found3-1)
+					overrideData.identity.facialHairType = imageString:sub(found2+1, found3-1)
 				end
 			end
 
-			if not self.identity.facialMaskType then
-				local found1, found2 = imageString:find("/"..(self.identity.facialMaskGroup or "facialMask").."/")
+			if not overrideData.identity.facialMaskType then
+				local found1, found2 = imageString:find("/"..(overrideData.identity.facialMaskGroup or "facialMask").."/")
 				if found1 ~= nil then
 					found3, found4 = imageString:find(".png")
-					self.identity.facialMaskType = imageString:sub(found2+1, found3-1)
+					overrideData.identity.facialMaskType = imageString:sub(found2+1, found3-1)
 				end
 			end
 		end
 	end
-	return self
+	return overrideData
 end
 
 function update(dt, fireMode, shiftHeld)
