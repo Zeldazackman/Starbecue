c7d47b7bde307fb8675183374b4a90cab2a3034e
WasabiRaptor <ketchupraptor@gmail.com>
2022-07-09 12:23:20 -0400

    fix that issue if the skin table is nil

--- vehicles/sbq/sbq_occupant_handling.lua
+++ vehicles/sbq/sbq_occupant_handling.lua
@@ -673,7 +673,7 @@ function sbq.doBellyEffects(dt)
 					end
 				end
 				if sbq.occupant[i].species and sbq.occupant[i].species ~= "sbqOccupantHolder" then
-					icon = "/vehicles/sbq/"..sbq.occupant[i].species.."/skins/"..(sbq.occupant[i].smolPreyData.settings.skinNames.head or "default").."/icon.png"..(sbq.occupant[i].smolPreyData.settings.directives or "")
+					icon = "/vehicles/sbq/"..sbq.occupant[i].species.."/skins/"..((sbq.occupant[i].smolPreyData.settings.skinNames or {}).head or "default").."/icon.png"..(sbq.occupant[i].smolPreyData.settings.directives or "")
 				end
 				sbq.loopedMessage(sbq.occupant[i].id.."-indicator", sbq.occupant[i].id, -- update quickly but minimize spam
 					"sbqOpenInterface", {"sbqIndicatorHud",
