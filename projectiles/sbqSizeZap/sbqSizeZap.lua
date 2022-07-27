c6d81f3a33a972654f043d23789d8f3d1005c8bd
WasabiRaptor <ketchupraptor@gmail.com>
2022-07-27 09:10:32 -0400

    micro dome NPCs, also make the size ray work on NPCs who don't already have the status applied

--- projectiles/sbqSizeZap/sbqSizeZap.lua
+++ projectiles/sbqSizeZap/sbqSizeZap.lua
@@ -1,5 +1,6 @@
 function uninit()
 	for i, id in ipairs(world.entityQuery(mcontroller.position(), 2) or {}) do
+		world.sendEntityMessage(id, "applySpeciesAnimOverride")
 		world.sendEntityMessage(id, "animOverrideScale", config.getParameter("animOverrideScale") or 1, config.getParameter("animOverrideScaleDuration") or 1 )
 	end
 end
