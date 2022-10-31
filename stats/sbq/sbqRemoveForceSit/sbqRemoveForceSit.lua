b38168a79005be701a88d04c3c1f833c0f7ad058
WasabiRaptor <ketchupraptor@gmail.com>
2022-10-31 16:25:22 -0400

    fix those animations

--- stats/sbq/sbqRemoveForceSit/sbqRemoveForceSit.lua
+++ stats/sbq/sbqRemoveForceSit/sbqRemoveForceSit.lua
@@ -1,11 +1,14 @@
 function init()
 	status.removeEphemeralEffect("sbqForceSit")
 	mcontroller.resetAnchorState()
+	mcontroller.setRotation(0)
 end
 
 function update(dt)
+	mcontroller.setRotation(0)
 	effect.expire()
 end
 
 function uninit()
+	mcontroller.setRotation(0)
 end
